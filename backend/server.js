472bf0845dbb8d1e90b6b2a096080ee4f977cd8a
Akhilsai <codeakhilsai@gmail.com>
2026-09-13 13:30:18 +0530

    cors added

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,11 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  }),
+);
 app.use(cookieParser());
 
 const PORT = process.env.PORT;
